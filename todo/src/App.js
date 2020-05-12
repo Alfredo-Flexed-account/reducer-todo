d6baa8b04c08f1cf82152a600ceb35d9459eb436
alfredo villasenor <alfredo.villasenor.aof@gmail.com>
2020-05-12 12:24:07 -0700

    finished with some styling

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,24 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState,useReducer,useEffect } from 'react';
+import { useForm } from 'react-hook-form';
 import './App.css';
+import TodoReducer, {initialState} from './reducer/TodoReducer';
+import TodoList from './TodoList';
 
 function App() {
+
+const {register} = useForm()
+const [todo,setTodo] = useState({message:''})
+const [state,dispatch] = useReducer(TodoReducer,initialState)
+
+const handleChange = e => {
+  setTodo({message: e.target.value})
+}
+
+ const handleSubmit = (e) => {
+    e.preventDefault()
+   dispatch({type: 'ADD_TODO', payload: {id:Date.now(),completed:false, message: todo.message } })
+ }
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+        <form onSubmit={handleSubmit}>
+          <input type="text" placeholder="add todo" ref={register} name="todo" value={todo.message} onChange={e => handleChange(e)}></input>
+          <input type="submit"></input>
+          <button type="button" onClick={() => dispatch({type: "CLEAR_COMPLETED"})}>clear completed</button>
+        </form>
+        <TodoList todos={state.todos} dispatch={dispatch}/>
     </div>
   );
 }
